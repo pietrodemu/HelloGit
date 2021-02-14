5d24cdc339675c7557bf44851c5737092bd30f42
pietrodemu <pietrodemu@hotmail.it>
2021-02-14 23:34:26 +0100

    A simple code with array and list

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace HelloGit
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Git!");
+            Console.WriteLine("Hello cat!");
+
+            var mylist = new List<int>() {0,1,2};
+            string[] alphabet = {"a","b","c"};
+
+            foreach (var item in mylist)
+            {
+                Console.WriteLine(alphabet[item]);
+            }
         }
     }
 }
